2c291ae9a0afef351521d690242486b03691fc70
Zaneh Williams <zanehmw@gmail.com>
2016-04-13 08:46:06 -0400

    what i have so far

--- app.rb
+++ app.rb
@@ -1,2 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+get '/' do
+  return "99 Bottles of beer on the wall"
+end
+
+get '/0' do
+  return "No Bottles of beer on the wall"
+end
+
+get '/:num_bottles' do
+  # testing
+return params[:num_bottles]
+end
