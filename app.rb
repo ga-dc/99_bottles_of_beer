c33a4829bdad2ee941e60686bdd741cc03c12e1c
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-12 23:07:57 -0400

    adds features

--- app.rb
+++ app.rb
@@ -2,16 +2,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  return "99 Bottles of beer on the wall. <a href='/98'>Take one down.</a>"
+  bottle_image = "<img src='http://www.spinnakers.com/media/beer-blank.png' alt='beeeeerrr height='100' width='46'/>" * 99
+  return "99 Bottles of beer on the wall. <a href='/98'>Take one down.</a><p>#{bottle_image}</p>"
 end
 
 get '/:num_bottles' do
   num_bottles = params[:num_bottles].to_i
   if num_bottles <= 0
     subtract_bottle = 0
-    return "No Bottles of beer on the wall. <a href='/'>Start Over</a>"
+    return "No Bottles of beer on the wall :-(. <a href='/'>Start Over</a>"
   else
     subtract_bottle = (num_bottles).pred
-    return "#{num_bottles} Bottles of beer on the wall. <a href='/#{subtract_bottle}'>Take one down.</a>"
+    bottle_image = "<img src='http://www.spinnakers.com/media/beer-blank.png' alt='beeeeerrr' height='100' width='46'/>" * num_bottles
+    return "#{num_bottles} Bottles of beer on the wall. <a href='/#{subtract_bottle}'>Take one down.</a><p>#{bottle_image}</p>"
   end
 end
