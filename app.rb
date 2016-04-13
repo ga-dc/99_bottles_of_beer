690a2e93d63df3f8d9a04a1b1af0fcf370f16f46
Mary Griffus <mary.e.shultz@gmail.com>
2016-04-12 20:36:30 -0400

    completed 99 bottles

--- app.rb
+++ app.rb
@@ -1,2 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
+@num_bottles = 99
+@message = 'Take one down'
+
+get '/' do
+  erb :index
+end
+
+get '/bottles/:num_bottles' do
+  @next_bottles = params[:num_bottles].to_i - 1
+  @num_bottles = params[:num_bottles].to_s
+  @message = 'Take one down'
+  if @next_bottles == -1
+    @num_bottles = 'No '
+    @message = ''
+  end
+  erb :index
+
+end
