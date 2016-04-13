e2934861065813c28f2b39398a6d11d3c860bbdd
Zaneh Williams <zanehmw@gmail.com>
2016-04-13 09:53:01 -0400

    finsihed

--- app.rb
+++ app.rb
@@ -2,14 +2,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  return "99 Bottles of beer on the wall"
+  @bottles = 99
+    erb(:index)
 end
 
-get '/0' do
-  return "No Bottles of beer on the wall"
-end
-
-get '/:num_bottles' do
-  # testing
-return params[:num_bottles]
+get '/bottles/:number' do
+  @bottles = params[:number].to_i
+  erb(:index)
 end
