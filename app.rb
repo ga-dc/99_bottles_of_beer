ff14a35e0a3391c7cba78cbdff1a7218b4c36338
Habby Whyte <bahybaj@hotmail.com>
2016-04-13 01:27:35 -0400

    initial commit- not much done

--- app.rb
+++ app.rb
@@ -1,2 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+get '/' do
+  @bottles =
+  erb(:index)
+end
+
+get '/:num_bottles' do
+  @bottles =
+  erb()
+end
+
+get '/0' do
+  @bottles =
+  erb(:0)
+end
