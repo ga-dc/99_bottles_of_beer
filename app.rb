d323166acc8cd72adb7f9deb7df5c8fe0bfe5f0d
Hewan Abreha <henebys@gmail.com>
2016-04-13 07:00:24 -0400

    added some solutions

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+# get '/:num_bottles' do
+#   "99 Bottles of beer on the wall. <a href=/'98'>Take one down</a>"
+# end
+get '/' do
+  @bottles =
+  erb(:index)
+end
+
 get '/:num_bottles' do
-  "99 Bottles of beer on the wall. <a href=/'98'>Take one down</a>"
+  @bottles =
+  erb()
+end
+
+get '/0' do
+  @bottles =
+  erb(:0)
 end
